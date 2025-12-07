971454fa668a8e5749705a97e04c76bce242382e
VaishnaviKanipakam <kanipakamvaishnavi1998@gmail.com>
2025-12-07 17:13:58 +0530

    Updates are added

--- app.js
+++ app.js
@@ -23,12 +23,12 @@ const initializeDbAndServer = async () => {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      port: Number(process.env.DB_PORT || 3306),
+      port:  3306,
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0,
     });
-     const port = process.env.DB_PORT || 3004;
+     const port = process.env.PORT || 3004;
     app.listen(port, () => {
       console.log(`app listening at ${port}...`);
     });
@@ -36,7 +36,7 @@ const initializeDbAndServer = async () => {
   if (err) {
     console.error("Database connection failed:", err.message);
   } else {
-    console.log("✅ Database connected successfully!");
+    console.log("Database connected successfully!");
     connection.release();
   }
 }); 
